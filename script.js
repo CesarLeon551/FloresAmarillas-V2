e621c4530660397968b72707f24c7a502bb82f92
CesarLeon551 <79882208+CesarLeon551@users.noreply.github.com>
2024-09-18 20:08:52 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 onload = () => {
   document.body.classList.remove("container");
 };
+
+document.addEventListener("DOMContentLoaded", function () {
+  const hiddenZone = document.getElementById("hidden-zone");
+  const closeButton = document.getElementById("close-hidden-zone");
+
+  // Mostrar la zona oculta al interactuar con un elemento
+  document.querySelector(".flowers").addEventListener("click", function () {
+    hiddenZone.style.display = "flex";
+  });
+
+  // Cerrar la zona oculta al hacer clic en el botón de cerrar
+  closeButton.addEventListener("click", function () {
+    hiddenZone.style.display = "none";
+  });
+});
